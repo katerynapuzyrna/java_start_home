5bb650aa35023b5a3e91558e40cebb9fa3462ff2
kateryna puzyrna <kateryna.puzyrna@gmail.com>
2019-02-19 18:50:36 +0200

    First commit from home environment

---
+++ src/lesson01/HelloWorld.java
@@ -0,0 +1,9 @@
+package lesson01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        byte a = 16;
+        System.out.println(a);
+    }
+}
